e434efa13d62534ccf591e67c7948ecbc197df6e
Aayush pandey <paayush1209@gmail.com>
2026-04-26 22:02:22 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(height) {
+        let x = 0,
+            y = height.length - 1;
+        let maxWater = 0;
+        while (x < y) {
+            let w = Math.min(height[x], height[y]) * (y - x);
+            maxWater = Math.max(maxWater, w);
+            if (height[x] >= height[y]) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return maxWater;
+    }
+}
