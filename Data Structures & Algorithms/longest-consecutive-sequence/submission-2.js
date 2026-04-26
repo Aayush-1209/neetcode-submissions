3ac5b283f076ff1415e2487e300c75913160c925
Aayush pandey <paayush1209@gmail.com>
2026-04-26 14:58:50 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const numSet = new Set(nums);
+
+        if (nums.length === 0) {
+            return 0;
+        }
+        let maxLen = 0;
+
+        for (let num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let currNum = num;
+                let currStreak = 1;
+
+                while (numSet.has(currNum + 1)) {
+                    currNum += 1;
+                    currStreak += 1;
+                }
+
+                maxLen = Math.max(maxLen, currStreak);
+            }
+        }
+
+        return maxLen;
+    }
+}
